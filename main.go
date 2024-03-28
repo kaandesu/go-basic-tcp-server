362bd70c0ba055bb07c706c34f17fd1b21226869
kaan <kaandesu00@gmail.com>
2024-03-28 18:24:40 +0100

    chore: added basic tcp conection

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+type Message struct {
+	from    string
+	payload []byte
+}
+
+type Server struct {
+	ln         net.Listener
+	quitch     chan struct{}
+	msgch      chan Message
+	listenAddr string
+}
+
+func NewServer(listenAddr string) *Server {
+	return &Server{
+		listenAddr: listenAddr,
+		quitch:     make(chan struct{}),
+		msgch:      make(chan Message, 10),
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.listenAddr)
+	if err != nil {
+		return err
+	}
+	defer ln.Close()
+	s.ln = ln
+
+	go s.acceptLoop()
+
+	<-s.quitch
+	close(s.msgch)
+
+	return nil
+}
+
+func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			fmt.Printf("accept error: %s", err)
+		}
+
+		fmt.Printf("new connection to the server: %s \n", conn.RemoteAddr())
+
+		go s.readLoop(conn)
+	}
+}
+
+func (s *Server) readLoop(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 2048)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Printf("read error: %s", err)
+			continue
+		}
+
+		msg := buf[:n]
+
+		s.msgch <- Message{
+			from:    conn.RemoteAddr().String(),
+			payload: msg,
+		}
+
+		conn.Write([]byte("message recieved!\n"))
+
+	}
+}
+
+func main() {
+	server := NewServer(":3000")
+
+	go func() {
+		for msg := range server.msgch {
+			fmt.Printf("reacieved message from connection (%s): %s", msg.from, string(msg.payload))
+		}
+	}()
+
+	log.Fatal(server.Start())
+}
